a4ffe86e420cff9b62e6a411f69825a3f154237e
nothrishit <hrishitjohn@gmail.com>
2023-08-31 14:24:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+import React from "react";
+import List from './components/List';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Reach me</h1>
+      <List />
+      {/* Code here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ListItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+// Complete this Component
+const ListItem = (props) => {
+  
+  // const {data} = this.props;
+  
+  return (
+    <div
+      className="ListItem"
+      style={{
+        height: 30,
+        backgroundColor : props.bgColor
+      }}
+    >
+      <img src={props.icon} alt={props.name} />
+      <a href={props.link}>{props.name}</a>
+    </div>
+  );
+};
+
+export default ListItem;
